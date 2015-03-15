65f5cc085773e115e6da9679de6793b10fa292ce
Spencer Piontkowski <spiontkowski@psrm.org>
2015-03-14 18:30:33 -0700

    Latest unix timestamp to cache parameter for scripts and styles.

--- psrm.php
+++ psrm.php
@@ -87,9 +87,9 @@ class PSRM
 
 	function enqueueScripts()
 	{
-		wp_enqueue_style(self::$slug . '-plugin-styles', self::$styles . '/main.min.css', [], '1424536731');
+		wp_enqueue_style(self::$slug . '-plugin-styles', self::$styles . '/main.min.css', [], '1426382961');
 
-		wp_register_script(self::$slug . '-plugin-scripts', self::$scripts . '/scripts.min.js', ['jquery'], '1424536731');
+		wp_register_script(self::$slug . '-plugin-scripts', self::$scripts . '/scripts.min.js', ['jquery'], '1426382961');
 		wp_localize_script( self::$slug . '-plugin-scripts', 'psrm', [
 			'ajaxurl' => admin_url('admin-ajax.php'),
 		] );
