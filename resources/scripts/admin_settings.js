ad394643d2035b036b68d0922ca6f7ac1c1bf7d9
Spencer Piontkowski <spiontkowski@psrm.org>
2015-09-13 11:48:36 -0700

    #15 - Admin.js sortable list needs to be conditional and only fire when the editable list is on the page.

--- resources/scripts/admin_settings.js
+++ resources/scripts/admin_settings.js
@@ -1,22 +1,25 @@
-jQuery(document).ready(function ($) {
+jQuery( document ).ready( function ( $ ) {
+    var $editable = document.getElementById( 'editable' );
+    if ( $editable ) {
 
-    var editableList = Sortable.create(document.getElementById('editable'), {
-        animation: 150,
-        filter: '.js-remove',
-        handle: '.drag-handle',
-        onFilter: function (evt) {
-            var el = editableList.closest(evt.item); // get dragged item
-            el.parentNode.removeChild(el);
-        }
-    });
+        var editableList = Sortable.create( $editable, {
+            animation: 150,
+            filter: '.js-remove',
+            handle: '.drag-handle',
+            onFilter: function ( evt ) {
+                var el = editableList.closest( evt.item ); // get dragged item
+                el.parentNode.removeChild( el );
+            }
+        } );
 
 
-    $('#add_amount_button').click(function (e) {
-        e.preventDefault();
-        var fieldName = $('#add_amount_name').val();
-        var amountAdded = $('#add_amount').val();
-        $('#editable').append('<li><input type="hidden" name="' + fieldName + '[]" value="' + amountAdded + '"><span class="drag-handle">&#9776;</span>' + amountAdded + ' <i class="js-remove">✖</i></li>');
-        $('#add_amount').val('');
-    });
+        $( '#add_amount_button' ).click( function ( e ) {
+            e.preventDefault();
+            var fieldName = $( '#add_amount_name' ).val();
+            var amountAdded = $( '#add_amount' ).val();
+            $( '#editable' ).append( '<li><input type="hidden" name="' + fieldName + '[]" value="' + amountAdded + '"><span class="drag-handle">&#9776;</span>' + amountAdded + ' <i class="js-remove">✖</i></li>' );
+            $( '#add_amount' ).val( '' );
+        } );
+    }
 
-});
+} );
