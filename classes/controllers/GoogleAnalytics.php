5684e316688e3ccb4a7cf691d75515da906c2eac
Spencer Piontkowski <spiontkowski@psrm.org>
2018-12-07 22:17:35 -0800

    Make some general improvements to the Google Analytics class

--- classes/controllers/GoogleAnalytics.php
+++ classes/controllers/GoogleAnalytics.php
@@ -7,23 +7,30 @@ use psrm\utils\View;
 class GoogleAnalytics
 {
 	public $cookie_name;
-	function __construct()
+
+	public function __construct()
 	{
 		$this->cookie_name = 'psrm_ga_exclude';
 		add_action('admin_init', array($this, 'tracking_exclusion_cookie'));
 		if(!isset($_COOKIE[$this->cookie_name])) {
-			add_action( 'wp_head', array( $this, 'tracking_code' ) );
+			add_action('wp_head', array($this, 'displayTrackingCode'), PHP_INT_MIN);
 		}
 	}
 
-	function tracking_exclusion_cookie()
+	/**
+	 * Set a cookie to remove the tracking code.
+	 */
+	public function tracking_exclusion_cookie()
 	{
 		if ( !isset($_COOKIE[$this->cookie_name])) {
 			setcookie( $this->cookie_name, 1, time() + 3600 * 24 * 365, COOKIEPATH, COOKIE_DOMAIN, false );
 		}
 	}
 
-	function tracking_code()
+	/**
+	 * Display the Google Analytics tracking code.
+	 */
+	public function displayTrackingCode()
 	{
 		echo new View('google-analytics');
 	}
