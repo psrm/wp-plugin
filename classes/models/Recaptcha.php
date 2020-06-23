0021f6a15aac4e4959b4db9a59b35c2ac20a9edf
Spencer Piontkowski <spiontkowski@psrm.org>
2020-06-22 22:13:24 -0700

    Remove recaptcha

--- classes/models/Recaptcha.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace psrm\models;
-
-class Recaptcha extends Settings {
-	const Group = 'recaptcha';
-	const SiteKeyOptionName = 'google_recaptcha_site_key';
-	const SecretKeyOptionName = 'google_recaptcha_secret_key';
-}
