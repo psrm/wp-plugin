e749edeadd8c5f5729af74f903ae1eb41315375f
Spencer Piontkowski <spiontkowski@psrm.org>
2020-07-24 16:16:49 -0700

    Update to the latest version of Stripe.js

--- psrm.php
+++ psrm.php
@@ -87,7 +87,8 @@ class PSRM
 
 		wp_enqueue_style(self::$slug . '-plugin-styles', self::$styles . '/main.css', [], '1468126460');
 
-		wp_register_script(self::$slug . '-plugin-scripts', self::$scripts . '/main.js', ['jquery'], '1545284377');
+		wp_enqueue_script('stripe-checkout', 'https://js.stripe.com/v3/');
+		wp_register_script(self::$slug . '-plugin-scripts', self::$scripts . '/main.js', ['jquery', 'stripe-checkout'], '1595630425');
 		wp_localize_script( self::$slug . '-plugin-scripts', 'psrm', [
 			'ajaxurl'   => admin_url( 'admin-ajax.php' ),
 			'name'      => get_bloginfo( 'name' ),
